5255ff74e203e6e9ac73ad192bd8861fc1a678d3
rparth69 <rparth69@gmail.com>
2019-09-30 19:01:19 +0530

    searchbutton

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'search' => 'search#index'
   devise_for :users
   resources :users, only: %i[show edit update]
   resources :posts, only: %i[new create show destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_26_092944) do
+ActiveRecord::Schema.define(version: 2019_09_30_102902) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,21 @@ ActiveRecord::Schema.define(version: 2019_09_26_092944) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "hash_tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_hash_tags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "hash_tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hash_tag_id"], name: "index_post_hash_tags_on_hash_tag_id"
+    t.index ["post_id"], name: "index_post_hash_tags_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
